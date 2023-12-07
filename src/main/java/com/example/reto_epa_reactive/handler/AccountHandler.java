6ce4de8786bb6a1022a0809d329690b166cf9fea
Sergio Nava <cerjionava@gmail.com>
2023-12-07 15:32:20 -0400

    feat: added handler for account services

---
+++ src/main/java/com/example/reto_epa_reactive/handler/AccountHandler.java
@@ -0,0 +1,36 @@
+package com.example.reto_epa_reactive.handler;
+
+import com.example.reto_epa_reactive.model.dto.ClientDTO;
+import com.example.reto_epa_reactive.usecase.CreateClientUseCase;
+import com.example.reto_epa_reactive.usecase.GetAllClientsUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AccountHandler {
+
+    @Autowired
+    private GetAllClientsUseCase getAllClientsUseCase;
+    @Autowired
+    private CreateClientUseCase createClientUseCase;
+
+    public Mono<ServerResponse> getAllClients (ServerRequest request) {
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromPublisher(getAllClientsUseCase.get(), ClientDTO.class));
+    }
+
+    public Mono<ServerResponse> createClient(ServerRequest request) {
+        return request.bodyToMono(ClientDTO.class)
+                .flatMap(clientDTO -> createClientUseCase.apply(clientDTO)
+                        .flatMap(result -> ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(result)));
+    }
+
+}
--- src/main/java/com/example/reto_epa_reactive/route/ClientRouter.java
+++ src/main/java/com/example/reto_epa_reactive/route/ClientRouter.java
@@ -1,18 +1,10 @@
 package com.example.reto_epa_reactive.route;
 
-import com.example.reto_epa_reactive.model.Client;
-import com.example.reto_epa_reactive.model.dto.ClientDTO;
-import com.example.reto_epa_reactive.usecase.CreateClientUseCase;
-import com.example.reto_epa_reactive.usecase.GetAllClientsUseCase;
+import com.example.reto_epa_reactive.handler.AccountHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Mono;
-
-import java.util.function.Function;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
@@ -21,29 +13,18 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class ClientRouter {
 
     @Bean
-    public RouterFunction<ServerResponse> getAllClients(GetAllClientsUseCase getAllClientsUseCase) {
+    public RouterFunction<ServerResponse> getAllClients(AccountHandler accountHandler) {
         return route(
                 GET("client/all"),
-                request ->
-                        ServerResponse.ok()
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .body(BodyInserters.fromPublisher(getAllClientsUseCase.get(), ClientDTO.class))
+                accountHandler::getAllClients
         );
     }
 
     @Bean
-    public RouterFunction<ServerResponse> createClient(CreateClientUseCase createClientUseCase) {
-        Function<ClientDTO, Mono<ServerResponse>> executor =
-                clientDTO ->
-                        createClientUseCase.apply(clientDTO)
-                        .flatMap(result ->
-                                ServerResponse.ok()
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .bodyValue(result));
-
+    public RouterFunction<ServerResponse> createClient(AccountHandler accountHandler) {
         return route(
-                POST("client/create").and(accept(MediaType.APPLICATION_JSON)),
-                request -> request.bodyToMono(ClientDTO.class).flatMap(executor)
+                POST("client/create"),
+                accountHandler::createClient
         );
     }
 }
